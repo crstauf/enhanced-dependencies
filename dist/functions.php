bf4b4486798a58741d71796ebeb6bb9a553be000
Caleb Stauffer <develop+github@calebstauffer.com>
2021-04-02 11:20:59 -0400

    add register and enqueue helper functions

--- dist/functions.php
+++ dist/functions.php
@@ -72,4 +72,100 @@ if ( !function_exists( 'wp_dehance_style' ) ) {
 
 }
 
+if ( !function_exists( 'wp_register_enhanced_script' ) ) {
+
+	/**
+	 * Register script and return Dependency object.
+	 *
+	 * Useful for immediate enhancement: wp_register_enhanced_script()->set( $enhancement_key )
+	 *
+	 * @param string $handle
+	 * @param string|bool $src
+	 * @param string[] $deps
+	 * @param string|bool|null $ver
+	 * @param bool $in_footer
+	 * @uses wp_register_script()
+	 * @uses Enhanced_Dependencies\Dependency::get()
+	 * @return Enhanced_Dependencies\Dependency
+	 *
+	 * @todo add tests
+	 */
+	function wp_register_enhanced_script( string $handle, $src, $deps = array(), $ver = false, $in_footer = false ) : Enhanced_Dependencies\Dependency {
+		wp_register_script( $handle, $src, $deps, $ver, $in_footer );
+		return Enhanced_Dependencies\Dependency::get( $handle, true );
+	}
+}
+
+if ( !function_exists( 'wp_enqueue_enhanced_script' ) ) {
+
+	/**
+	 * Enqueue script and return Dependency object.
+	 *
+	 * Useful for immediate and conditional enhancement: wp_enqueue_enhanced_script()->set( $enhancement_key )
+	 *
+	 * @param string $handle
+	 * @param string|bool $src
+	 * @param string[] $deps
+	 * @param string|bool|null $ver
+	 * @param bool $in_footer
+	 * @uses wp_enqueue_script()
+	 * @uses Enhanced_Dependencies\Dependency::get()
+	 * @return Enhanced_Dependencies\Dependency
+	 *
+	 * @todo add tests
+	 */
+	function wp_enqueue_enhanced_script( string $handle, $src, $deps = array(), $ver = false, $in_footer = false ) : Enhanced_Dependencies\Dependency {
+		wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
+		return Enhanced_Dependencies\Dependency::get( $handle, true );
+	}
+}
+
+if ( !function_exists( 'wp_register_enhanced_style' ) ) {
+
+	/**
+	 * Register stylesheet and return Dependency object.
+	 *
+	 * Useful for immediate enhancement: wp_register_enhanced_style()->set( $enhancement_key )
+	 *
+	 * @param string $handle
+	 * @param string|bool $src
+	 * @param string[] $deps
+	 * @param string|bool|null $ver
+	 * @param string $media
+	 * @uses wp_register_style()
+	 * @uses Enhanced_Dependencies\Dependency::get()
+	 * @return Enhanced_Dependencies\Dependency
+	 *
+	 * @todo add tests
+	 */
+	function wp_register_enhanced_style( string $handle, $src, $deps = array(), $ver = false, $media = 'all' ) : Enhanced_Dependencies\Dependency {
+		wp_register_style( $handle, $src, $deps, $ver, $media );
+		return Enhanced_Dependencies\Dependency::get( $handle, false );
+	}
+}
+
+if ( !function_exists( 'wp_enqueue_enhanced_style' ) ) {
+
+	/**
+	 * Enqueue stylesheet and return Dependency object.
+	 *
+	 * Useful for immediate enhancement: wp_enqueue_enhanced_script()->set( $enhancement_key )
+	 *
+	 * @param string $handle
+	 * @param string|bool $src
+	 * @param string[] $deps
+	 * @param string|bool|null $ver
+	 * @param string $media
+	 * @uses wp_enqueue_style()
+	 * @uses Enhanced_Dependencies\Dependency::get()
+	 * @return Enhanced_Dependencies\Dependency
+	 *
+	 * @todo add tests
+	 */
+	function wp_enqueue_enhanced_style( string $handle, $src, $deps = array(), $ver = false, $media = 'all' ) : Enhanced_Dependencies\Dependency {
+		wp_enqueue_style( $handle, $src, $deps, $ver, $media );
+		return Enhanced_Dependencies\Dependency::get( $handle, false );
+	}
+}
+
 ?>
--- tests/php/test-functions.php
+++ tests/php/test-functions.php
@@ -75,6 +75,54 @@ class Test_Functions extends \WP_UnitTestCase {
 		$this->_test_dependency_data( wp_styles(), $test_handle, $test_enhancement_key );
 	}
 
+	function test_wp_register_enhanced_script() : void {
+		$test_handle = uniqid( 'test-enhancement-script' );
+		$builtin = wp_scripts()->registered['jquery-core'];
+		$test_enhancement_key = 'test-script-enhancement';
+		$options = array( 'test-opt' => rand() );
+
+		wp_register_enhanced_script( $test_handle, $builtin->src, $builtin->deps, $builtin->ver )->set( $test_enhancement_key, $options );
+
+		$this->assertTrue( wp_script_is( $test_handle, 'registered' ) );
+		$this->_test_dependency_data( wp_scripts(), $test_handle, $test_enhancement_key, $options );
+	}
+
+	function test_wp_enqueue_enhanced_script() : void {
+		$test_handle = uniqid( 'test-enhancement-script' );
+		$builtin = wp_scripts()->registered['jquery-core'];
+		$test_enhancement_key = 'test-script-enhancement';
+		$options = array( 'test-opt' => rand() );
+
+		wp_enqueue_enhanced_script( $test_handle, $builtin->src, $builtin->deps, $builtin->ver )->set( $test_enhancement_key, $options );
+
+		$this->assertTrue( wp_script_is( $test_handle, 'enqueued' ) );
+		$this->_test_dependency_data( wp_scripts(), $test_handle, $test_enhancement_key, $options );
+	}
+
+	function test_wp_register_enhanced_style() : void {
+		$test_handle = uniqid( 'test-enhancement-style' );
+		$builtin = wp_styles()->registered['admin-bar'];
+		$test_enhancement_key = 'test-style-enhancement';
+		$options = array( 'test-opt' => rand() );
+
+		wp_register_enhanced_style( $test_handle, $builtin->src, $builtin->deps, $builtin->ver )->set( $test_enhancement_key, $options );
+
+		$this->assertTrue( wp_style_is( $test_handle, 'registered' ) );
+		$this->_test_dependency_data( wp_styles(), $test_handle, $test_enhancement_key, $options );
+	}
+
+	function test_wp_enqueue_enhanced_style() : void {
+		$test_handle = uniqid( 'test-enhancement-style' );
+		$builtin = wp_styles()->registered['admin-bar'];
+		$test_enhancement_key = 'test-style-enhancement';
+		$options = array( 'test-opt' => rand() );
+
+		wp_enqueue_enhanced_style( $test_handle, $builtin->src, $builtin->deps, $builtin->ver )->set( $test_enhancement_key, $options );
+
+		$this->assertTrue( wp_style_is( $test_handle, 'enqueued' ) );
+		$this->_test_dependency_data( wp_styles(), $test_handle, $test_enhancement_key, $options );
+	}
+
 }
 
 ?>
